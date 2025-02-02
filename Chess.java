1d8c1c4691e46fbd2193a251e37c6f7753c9ff3a
Conra6 <conrado.andres@hotmail.com>
2025-02-02 12:23:50 -0700

    Update Chess.java

--- Chess.java
+++ Chess.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 import java.io.File;
 public class Chess{
 	public static void main(String[]args){
+		Scanner scn = new Scanner(System.in);
+		//User menu selection for the next movement, the user will choose the piece, and the both coodinates 
+		//piece selection
+		System.out.println("Choose the piece you want to move. Options: King, Rook, Pawn, Knight, Queen, Bishop")
+		String ChoosePiece=scn.nextLine();
+		//x selection
+		System.out.println("Choose the x coordinate(A-H)");
+		String ChooseX=scn.nextLine();
+		//y selection
+		System.out.println("Choose the y coordinate(1-8)");
+		int ChooseY=scn.nextInt();
+		
 		try{
 			//Reads and save the text file into an 2d array
 			Scanner sc = new Scanner(new File("chess.txt")); 
@@ -14,19 +26,13 @@ public class Chess{
 				System.out.println(database[i][0]+"|"+database[i][1]+"|"+database[i][2]+"|"+database[i][3]);
 				i++;
 			}
-			
+			sc.close();
 			
 		}
 		catch(Exception e){
 			System.out.println("File can't be read");
 		}
-		//User menu selection for the next movement, the user will choose the piece, and the both coodinates 
-		System.out.println("Choose the piece you want to move. Options: King, Rook, Pawn, Knight, Queen, Bishop")
-		//String ChoosePiece= ;
-		System.out.println("Choose the x coordinate(A-H)");
-		//String
-		System.out.println("Choose the y coordinate(1-8)");
-		//int
+		
 	}
 
 }
