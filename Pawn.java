9c23b09f1fd7bbeb694ba6da35be600b0612dac3
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2025-02-19 19:26:10 -0700

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -1,29 +1,11 @@
 class Pawn extends ChessPiece {
-    public Pawn(ColorType color, char column, int row) {
-        super(PieceType.PAWN, color, column, row);
+    // Empty constructor
+    public Pawn(String color, char column, int row) {
+        super("Pawn", color, column, row);
     }
 
     @Override
-    public boolean verifyTarget(char targetCol, int targetRow) {
-        int direction = (color == ColorType.WHITE) ? 1 : -1; // White moves up, Black moves down
-        int rowDiff = targetRow - row;
-        int colDiff = Math.abs(targetCol - column);
-
-        // Regular move: One step forward
-        if (colDiff == 0 && rowDiff == direction) {
-            return true;
-        }
-
-        // First move: Two steps forward from starting row
-        if (colDiff == 0 && rowDiff == 2 * direction && (row == 2 || row == 7)) {
-            return true;
-        }
-
-        // Capturing move: One step diagonally forward
-        if (colDiff == 1 && rowDiff == direction) {
-            return true;
-        }
-
-        return false;
+    public boolean verifyMove(char newX, int newY) {
+        return (this.column == newX) && (newY == this.row + (color.equals("WHITE") ? 1 : -1));
     }
 }
